1995e8180f849450c6c17fdaa848e90ba1621b8b
bbest <bbest@nceas.ucsb.edu>
2015-06-15 13:35:13 -0700

    comments and corrections

--- prep/install_packages.R
+++ prep/install_packages.R
@@ -1,11 +1,18 @@
+
+# get packages to run installs
 for (pkg in c('readr','stringr','dplyr','devtools')){
   if (!require(pkg)) install.packages(pkg)
   require(pkg)
 }
+
+# read output from as.list(devtools::session_info())[[2]]
 p = read_csv('https://raw.githubusercontent.com/bbest/consmap/master/prep/R_packages.csv') %>%
   mutate(
     github = str_match(source, "Github \\((.*)\\)")[,2]) %>%
   arrange(!is.na(github), package, github)
+
+# iterate through packages
+#   installing from CRAN or Github as needed
 for (i in 1:nrow(p)){
   pkg = p$package[i]
   gh  = p$github[i]
@@ -17,3 +24,6 @@ for (i in 1:nrow(p)){
     }
   } 
 }
+
+# run Shiny app from Github
+shiny::runGitHub('bbest/consmap')
