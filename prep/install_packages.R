67e8206642d49410f853260e36f0edf527ca4b7e
bbest <bbest@nceas.ucsb.edu>
2015-06-15 13:29:08 -0700

    fixed install_github

--- prep/install_packages.R
+++ prep/install_packages.R
@@ -8,11 +8,12 @@ p = read_csv('https://raw.githubusercontent.com/bbest/consmap/master/prep/R_pack
   arrange(!is.na(github), package, github)
 for (i in 1:nrow(p)){
   pkg = p$package[i]
+  gh  = p$github[i]
   if (!require(pkg)){
-    if (is.na(p$github[i])){
+    if (is.na(gh)){
       install.packages(pkg)
     } else {
-      install_github(pkg)
+      install_github(gh)
     }
   } 
 }
