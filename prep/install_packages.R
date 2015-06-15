0d0965fdbd8c09530ff3075b019bc9e6e7a74dc8
bbest <bbest@nceas.ucsb.edu>
2015-06-15 13:48:27 -0700

    more comments, require(pkg, character.only=T)

--- prep/install_packages.R
+++ prep/install_packages.R
@@ -1,7 +1,14 @@
+# install R packages necessary to run Shiny app
+# TODO: check for essential version of leaflet
 
 # get packages to run installs
 for (pkg in c('readr','stringr','dplyr','devtools')){
-  if (!require(pkg, character.only=T)) install.packages(pkg)
+  if (!require(pkg, character.only=T)){
+    cat(sprintf('----\n%s: INSTALLING from CRAN\n...\n', pkg))
+    install.packages(pkg)
+  }  else {
+    cat(sprintf('----\n%s: already installed\n', pkg))
+  }
   library(pkg, character.only=T)
 }
 
@@ -16,15 +23,17 @@ p = read_csv('https://raw.githubusercontent.com/bbest/consmap/master/prep/R_pack
 for (i in 1:nrow(p)){
   pkg = p$package[i]
   gh  = p$github[i]
-  if (!require(pkg)){
+  if (!require(pkg, character.only=T)){
     if (is.na(gh)){
-      cat(sprintf('----\nINSTALLING from CRAN: %s\n...\n', pkg))
+      cat(sprintf('----\n%s: INSTALLING from CRAN\n...\n', pkg))
       install.packages(pkg)
     } else {
-      cat(sprintf('----\nINSTALLING from GITHUB: %s\n...\n', gh))
+      cat(sprintf('----\n%s: INSTALLING from GITHUB (%s)\n...\n', pkg, gh))
       install_github(gh)
     }
-  } 
+  } else {
+    cat(sprintf('----\n%s: already installed\n', pkg))
+  }
 }
 
 # run Shiny app from Github
